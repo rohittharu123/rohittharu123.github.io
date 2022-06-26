dc53de991bfb300eb8c391d2673e72727825cc7c
RohitTharu17 <tharurohit98@gmail.com>
2022-06-26 13:38:21 +0545

    deploy portfolio

---
+++ js/education.js
@@ -0,0 +1,72 @@
+AOS.init();
+
+let currentItem = 0;
+
+const img = document.getElementById("image");
+
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+window.addEventListener("DOMContentLoaded", function () {
+  showExperience();
+});
+
+function showExperience() {
+  setInterval(function () {
+    if (currentItem === experience.length) {
+      currentItem = 0;
+    }
+    const item = experience[currentItem];
+    img.src = item.img;
+    currentItem++;
+  }, 3000);
+}
+
+/* Timeline Section*/
+
+$(function () {
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+    if ($(".timeline-content").hasClass("js--fadeInLeft")) {
+      $(".timeline-content")
+        .removeClass("js--fadeInLeft")
+        .addClass("js--fadeInRight");
+    }
+
+    sr.reveal(".js--fadeInRight", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
+  } else {
+    sr.reveal(".js--fadeInLeft", {
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
+
+    sr.reveal(".js--fadeInRight", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
+  }
+
+  sr.reveal(".js--fadeInLeft", {
+    origin: "left",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800,
+  });
+
+  sr.reveal(".js--fadeInRight", {
+    origin: "right",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800,
+  });
+});
